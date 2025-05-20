450c83755949b43eb0160c18d7393207d5cbff6e
Liang Qi Yuan <1710178776@qq.com>
2025-05-21 02:08:17 +0800

    refactor: 优化分页组件，不使用 v-model 绑定页数和条数

--- common/components/CommonPagination/index.vue
+++ common/components/CommonPagination/index.vue
@@ -1,12 +1,17 @@
 <script setup lang="ts">
 import type { CommonPaginationProps } from './type'
-import { watch, watchEffect } from 'vue'
 import { CommonPaginationConfig } from './config'
 
 defineOptions({
   name: 'CommonPagination',
 })
-const props = withDefaults(defineProps<CommonPaginationProps>(), {
+
+const props = withDefaults(defineProps<CommonPaginationProps & {
+  /** 当前页码 */
+  currentPage?: number,
+  /** 每页条数 */
+  pageSize?: number
+}>(), {
   /** 总数 */
   total: 0,
   /** 带有背景色的分页 */
@@ -17,46 +22,101 @@ const props = withDefaults(defineProps<CommonPaginationProps>(), {
   layout: 'slot, ->, total, sizes, prev, pager, next, jumper',
   /** 每页显示个数选择器的选项设置 */
   pageSizes: () => [10, 20, 30, 40, 50],
+  /** 当前页码 */
+  currentPage: CommonPaginationConfig.defaultCurrentPage,
+  /** 每页条数 */
+  pageSize: CommonPaginationConfig.defaultPageSize
 })
 
 const emit = defineEmits<{
+  /** 页码变化 */
+  'update:currentPage': [number],
+  /** 每页条数变化 */
+  'update:pageSize': [number],
+  /** 分页变化 */
   change: [{
     currentPage: number
     pageSize: number
   }]
 }>()
 
-const currentPage = defineModel('currentPage', {
-  type: Number,
-  default: CommonPaginationConfig.defaultCurrentPage,
-})
-const pageSize = defineModel('pageSize', {
-  type: Number,
-  default: CommonPaginationConfig.defaultPageSize,
-})
+/**
+ * 内部当前页码
+ */
+const innerCurrentPage = ref(props.currentPage)
 
-watch(() => pageSize.value, () => {
-  currentPage.value = 1
-})
+/**
+ * 内部每页条数
+ */
+const innerPageSize = ref(props.pageSize)
 
-watchEffect(() => {
-  emit('change', {
-    currentPage: currentPage.value,
-    pageSize: pageSize.value,
-  })
-})
-
-defineExpose({
-  reset() {
-    currentPage.value = CommonPaginationConfig.defaultCurrentPage
-    pageSize.value = CommonPaginationConfig.defaultPageSize
+/**
+ * 监听属性变化
+ */
+watch(() => props.currentPage, (val) => {
+  if (val !== innerCurrentPage.value) {
+    innerCurrentPage.value = val
   }
+})
+
+watch(() => props.pageSize, (val) => {
+  if (val !== innerPageSize.value) {
+    innerPageSize.value = val
+  }
+})
+
+/**
+ * 处理页码变化
+ */
+function handleCurrentPageChange(page: number) {
+  innerCurrentPage.value = page
+  emit('update:currentPage', page)
+  emitChange()
+}
+
+/**
+ * 处理每页条数变化
+ */
+function handlePageSizeChange(size: number) {
+  innerPageSize.value = size
+  // 当每页条数变化时，重置为第一页
+  innerCurrentPage.value = 1
+  emit('update:pageSize', size)
+  emit('update:currentPage', 1)
+  emitChange()
+}
+
+/**
+ * 触发变化事件
+ */
+function emitChange() {
+  emit('change', {
+    currentPage: innerCurrentPage.value,
+    pageSize: innerPageSize.value
+  })
+}
+
+/**
+ * 重置分页
+ */
+function reset() {
+  innerCurrentPage.value = CommonPaginationConfig.defaultCurrentPage
+  innerPageSize.value = CommonPaginationConfig.defaultPageSize
+  emit('update:currentPage', CommonPaginationConfig.defaultCurrentPage)
+  emit('update:pageSize', CommonPaginationConfig.defaultPageSize)
+  emitChange()
+}
+
+// 暴露方法
+defineExpose({
+  reset
 })
 
 </script>
 
 <template>
-  <el-pagination v-bind="props" v-model:page-size="pageSize" v-model:current-page="currentPage">
+  <el-pagination v-bind="props" :page-size="innerPageSize" :current-page="innerCurrentPage"
+    @update:current-page="handleCurrentPageChange" @update:page-size="handlePageSizeChange">
     <slot />
   </el-pagination>
 </template>
