f3ec07875ff2907901aef9bcc92ac58741e41ca3
Liang Qi Yuan <1710178776@qq.com>
2025-05-25 02:55:59 +0800

    chore: 更新自动生成的类型文件
    - 更新 auto-imports.d.ts、components.d.ts、typed-router.d.ts 等
    - 反映最新的组件和路由配置变更
    - 自动生成文件，保持类型定义同步

--- typings/vue-router.d.ts
+++ typings/vue-router.d.ts
@@ -1,18 +1,19 @@
-import type { Router } from 'vue-router'
+import type { RouteLocationNormalizedLoaded, Router } from 'vue-router'
 
 export {}
-
 declare module 'vue-router' {
+  /** 这里仅作类型提示，若 类型为必填类型，则需要在 vite.config VueRouter.extendRoute 中显示声明 */
   interface RouteMeta {
     /** 标题 */
     title?: string
     /** 是否在 layout 中显示 默认为 true */
-    layout?: boolean
+    layout: boolean
   }
 }
 
 declare module '@vue/runtime-core' {
   interface ComponentCustomProperties {
     $router: Router
+    $route: RouteLocationNormalizedLoaded
   }
 }
