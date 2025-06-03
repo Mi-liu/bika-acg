886bb2153acf198fa85c4504bd7960dfa99d0365
Liang Qi Yuan <1710178776@qq.com>
2025-06-03 16:49:35 +0800

    chore: 更新自动导入类型定义

--- typings/auto-imports.d.ts
+++ typings/auto-imports.d.ts
@@ -321,4 +321,7 @@ declare global {
   // @ts-ignore
   export type { Component, Slot, Slots, ComponentPublicInstance, ComputedRef, DirectiveBinding, ExtractDefaultPropTypes, ExtractPropTypes, ExtractPublicPropTypes, InjectionKey, PropType, Ref, MaybeRef, MaybeRefOrGetter, VNode, WritableComputedRef } from 'vue'
   import('vue')
+  // @ts-ignore
+  export type { Local } from '../src/store/modules/local'
+  import('../src/store/modules/local')
 }
