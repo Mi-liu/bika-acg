8858176d8ad748b9a35160c65bf4a3d2bbd4977c
Liang Qi Yuan <1710178776@qq.com>
2025-06-03 16:48:52 +0800

    feat: 新增状态管理插件和更新store配置

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,3 +1,15 @@
-import { createPinia } from 'pinia'
+import { createPinia, type PiniaPluginContext } from 'pinia'
 
-export const store = createPinia()
+const store = createPinia()
+
+// 使用 Vite 的 import.meta.globEager 一键导入pinia所有插件
+const modules = import.meta.glob<{ default: (context: PiniaPluginContext) => void }>(
+  './plugin/*.ts',
+  { eager: true },
+)
+
+Object.entries(modules).forEach(([, value]) => {
+  store.use(value.default)
+})
+
+export { store }
