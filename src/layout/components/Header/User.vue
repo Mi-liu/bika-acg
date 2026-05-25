bcabd24bf9613fa4dc94a6ea2754c3df19300f17
WIN-20260508IVK\Administrator <1710178776@qq.com>
2026-05-25 11:04:59 +0800

    fix: 支持用户菜单链接新标签打开

--- src/layout/components/Header/User.vue
+++ src/layout/components/Header/User.vue
@@ -7,48 +7,14 @@ userStore.getUserProfile()
 
 const router = useRouter()
 
-function handleFavourite() {
-  router.push({
-    path: '/user/favorites',
-  })
-}
-
-function handleComments() {
-  router.push({
-    path: '/user/comments',
-  })
-}
-
-function handleFollowing() {
-  router.push({
-    path: '/user/following',
-  })
-}
-
-function handleWatchLater() {
-  router.push({
-    path: '/user/watch-later',
-  })
-}
-
-function handleSetting() {
-  router.push({
-    path: '/user/setting',
-  })
-}
-
-/**
- * 处理切换账号
- * 跳转到账号列表页面
- */
-function handleSwitchAccount() {
-  router.replace({
+const switchAccountRoute = computed(() => {
+  return {
     path: '/login/account-list',
     query: {
       redirect: encodeURIComponent(router.currentRoute.value.fullPath),
     },
-  })
-}
+  }
+})
 
 /**
  * 处理退出登录
@@ -92,16 +58,83 @@ function handleLogout() {
     </el-button>
     <template #dropdown>
       <el-dropdown-menu>
-        <el-dropdown-item :icon="Link" @click="handleFollowing">我的关注</el-dropdown-item>
-        <el-dropdown-item :icon="Star" @click="handleFavourite">我的收藏</el-dropdown-item>
-        <el-dropdown-item :icon="ChatLineSquare" @click="handleComments">我的评论</el-dropdown-item>
-        <el-dropdown-item :icon="Timer" @click="handleWatchLater">稍后再看</el-dropdown-item>
-        <el-dropdown-item :icon="Setting" @click="handleSetting">设置</el-dropdown-item>
-        <el-dropdown-item :icon="UserFilled" divided @click="handleSwitchAccount">切换账号</el-dropdown-item>
-        <el-dropdown-item :icon="SwitchButton" @click="handleLogout">退出登录</el-dropdown-item>
+        <el-dropdown-item class="user-menu-link-item">
+          <RouterLink class="user-menu-link" to="/user/following">
+            <el-icon>
+              <Link />
+            </el-icon>
+            <span>我的关注</span>
+          </RouterLink>
+        </el-dropdown-item>
+        <el-dropdown-item class="user-menu-link-item">
+          <RouterLink class="user-menu-link" to="/user/favorites">
+            <el-icon>
+              <Star />
+            </el-icon>
+            <span>我的收藏</span>
+          </RouterLink>
+        </el-dropdown-item>
+        <el-dropdown-item class="user-menu-link-item">
+          <RouterLink class="user-menu-link" to="/user/comments">
+            <el-icon>
+              <ChatLineSquare />
+            </el-icon>
+            <span>我的评论</span>
+          </RouterLink>
+        </el-dropdown-item>
+        <el-dropdown-item class="user-menu-link-item">
+          <RouterLink class="user-menu-link" to="/user/watch-later">
+            <el-icon>
+              <Timer />
+            </el-icon>
+            <span>稍后再看</span>
+          </RouterLink>
+        </el-dropdown-item>
+        <el-dropdown-item class="user-menu-link-item">
+          <RouterLink class="user-menu-link" to="/user/setting">
+            <el-icon>
+              <Setting />
+            </el-icon>
+            <span>设置</span>
+          </RouterLink>
+        </el-dropdown-item>
+        <el-dropdown-item class="user-menu-link-item" divided>
+          <RouterLink class="user-menu-link" :to="switchAccountRoute" replace>
+            <el-icon>
+              <UserFilled />
+            </el-icon>
+            <span>切换账号</span>
+          </RouterLink>
+        </el-dropdown-item>
+        <el-dropdown-item class="user-menu-link-item" @click="handleLogout">
+          <span class="user-menu-link">
+            <el-icon>
+              <SwitchButton />
+            </el-icon>
+            <span>退出登录</span>
+          </span>
+        </el-dropdown-item>
       </el-dropdown-menu>
     </template>
   </el-dropdown>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.user-menu-link-item {
+  padding: 0;
+
+  :deep(.el-dropdown-menu__item) {
+    padding: 0;
+  }
+}
+
+.user-menu-link {
+  display: flex;
+  align-items: center;
+  gap: 6px;
+  width: 100%;
+  padding: 5px 16px;
+  color: inherit;
+  text-decoration: none;
+}
+</style>
