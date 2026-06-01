56cb3f5932e87e12cae299e2e67439a648cc1694
WIN-20260508IVK\Administrator <1710178776@qq.com>
2026-06-01 10:15:13 +0800

    fix: 隐藏顶部天气位置名称

--- src/layout/components/Header/Weather.vue
+++ src/layout/components/Header/Weather.vue
@@ -44,7 +44,7 @@ const nowWeather = computed(() => data.value)
 
 <template>
   <div v-if="!isEmpty(nowWeather)" class="flex items-center select-none">
-    {{ nowWeather.location.name }}
+    <!-- {{ nowWeather.location.name }} -->
     {{ nowWeather.now.text }}
     {{ nowWeather.now.temperature }}°C
     <img class="h-24px ml-1" :src="weatherImagesMap.get(nowWeather.now.code)">
